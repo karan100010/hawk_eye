8577a2d62c34f9aa5891dbb04d0cd04ba8c8dcee
karan100010 <karanjoshi1993@gmail.com>
2022-02-28 16:38:06 +0530

    added hindi

--- krishma/karishmabot/karishmaxpal/karishmatg/karishmatgbot.py
+++ krishma/karishmabot/karishmaxpal/karishmatg/karishmatgbot.py
@@ -130,7 +130,7 @@ def loop(update: Update, context: CallbackContext):
 
         
         try:
-            text=pytesseract.image_to_string(Image.open("image.jpg"),lang="Devanagari+eng")
+            text=pytesseract.image_to_string(Image.open("image.jpg"),lang="hin+eng")
             #add utf-8 encoding to text
           
             logger.info("Downloading image {}".format(text))

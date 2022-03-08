3a295c32632cbe73d62c0a31f96f5e42041d39b7
karan100010 <karanjoshi1993@gmail.com>
2022-03-08 19:25:05 +0530

    initial working api up

--- api/main.py
+++ api/main.py
@@ -3,7 +3,8 @@ from typing import List
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
-import crud, models
+
+import crud, models,schemas
 from database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
---
+++ api/schemas.py
@@ -0,0 +1,67 @@
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+#write schemas for NewsItems
+class NewsItems(BaseModel):
+    id: int
+    title: str
+    link: str
+    text: str
+    state: str
+    category: str
+    subtheme: str
+    data_scraped: str
+    data_published: str
+    language: str
+    location: str
+    long: str
+    lat: str
+    quotes: str
+    image_num: int
+    image_url: str
+    image_found: bool
+    publication: str
+
+ # write schemas for NewsItemsUpdate
+class NewsItemsUpdate(BaseModel):
+    title: Optional[str] = None
+    link: Optional[str] = None
+    text: Optional[str] = None
+    state: Optional[str] = None
+    category: Optional[str] = None
+    subtheme: Optional[str] = None
+    data_scraped: Optional[str] = None
+    data_published: Optional[str] = None
+    language: Optional[str] = None
+    location: Optional[str] = None
+    long: Optional[str] = None
+    lat: Optional[str] = None
+    quotes: Optional[str] = None
+    image_num: Optional[int] = None
+    image_url: Optional[str] = None
+    image_found: Optional[bool] = None
+    publication: Optional[str] = None   
+
+#write schemas for NewsItemsCreate
+class NewsItemsCreate(BaseModel):
+    title: str
+    link: str
+    text: str
+    state: str
+    category: str
+    subtheme: str
+    data_scraped: str
+    data_published: str
+    language: str
+    location: str
+    long: str
+    lat: str
+    quotes: str
+    image_num: int
+    image_url: str
+    image_found: bool
+    publication: str
+
+ # write schemas for NewsItemsInDB
+            

8e69f342afe73fd7d7a74879e4c5bb623c6f30b3
karan100010 <karanjoshi1993@gmail.com>
2022-03-08 16:00:12 +0530

    included the changed paths in database.py

--- api/database.py
+++ api/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from base import sql_login
+from ..base import sql_login
 
 
 engine = sql_login("scraper.conf")

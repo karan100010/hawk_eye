f8b48a184d6c1aa8ef03ae22d5316ccb13841563
karan100010 <karanjoshi1993@gmail.com>
2022-07-06 16:59:07 +0530

    added pos to daily scipts

--- daily_script.py
+++ daily_script.py
@@ -2,7 +2,7 @@ import pandas
 from base import *
 from themes import theme_dict
 from datetime import datetime
-
+import stanza
 def get_data():
     all_data=[]
     for i in theme_dict:
@@ -22,7 +22,30 @@ df=pandas.DataFrame(all_data_clean)
 df=filter_text(df)
 df=check_duplicate_text(df)
 df=check_duplicate_links(df)
+df=remove_brackets_regex(df)
 name="daily_data"+datetime.now().strftime("%Y-%m-%d")+".csv"
+df=df[~df["text"].str.lower().str.contains("gyanvapi")]
+
 
 #get dataframe to csv with the time stamp of the day
 df.to_csv(name,index=False)
+
+nlp=stanza.Pipeline(lang="en",processors='tokenize,pos')
+
+df_hi=df[df["language"]=="hi"]
+df_en=df[df["language"]=="en"]
+df_en["pos"]=df_en.text.apply(get_pos_from_text_hi,nlp=nlp)
+nlp=stanza.Pipeline(lang="en",processors='tokenize,pos')
+df_hi["pos"]=df_hi.text.apply(get_pos_from_text_hi,nlp=nlp)
+
+df=pandas.concat([df_hi,df_en])
+
+df["noun"]=df.pos.apply(get_nouns)
+df["propnouns"]=df.pos.apply(get_propernouns)
+df["word_count_nouns"]=df.noun.apply(get_wordcloud_string)
+df["word_count_propnouns"]=df.propnouns.apply(get_wordcloud_string)
+df["word_count_nouns"]=df.word_count_nouns.str.replace("ad","")
+df["dates"]=df.date_published.apply(lambda x: x.split()[0])
+dff=pandas.read_csv("final_may.csv")
+dfx=pandas.concat(df,dff)
+dfx.to_csv("final_may.csv",index=False)
